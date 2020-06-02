6299d66ff68ed922b19bd11377532b1f9061d3eb
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-02 21:52:53 +0200

    Added a service with a BehaviourSubject

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,49 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent implements OnInit {
-  constructor() {}
+export class HomePageComponent implements OnInit, OnDestroy {
+  constructor(private service: ProductService) {}
 
-  products: Product[] = [
-    {
-      id: '1',
-      title: 'Voda',
-      price: 40,
-      image:
-        'https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-    },
-    {
-      id: '2',
-      title: 'Sok',
-      price: 60,
-      image: 'https://nsnbc.me/wp-content/uploads/2019/07/fruit-juice.jpg',
-    },
-    {
-      id: '3',
-      title: 'Špagete',
-      price: 80,
-      image:
-        'https://pbs.twimg.com/profile_images/423290202443251712/HYR23k2c.jpeg',
-    },
-    {
-      id: '4',
-      title: 'Makarone',
-      price: 90,
-      image: 'https://bonapeti.rs/files/lib/600x350/makaroni-vkusensos1.jpg',
-    },
-  ];
+  products: Product[] = [];
+  subscription: Subscription;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.service.productSubject.subscribe(
+      (products) => (this.products = products)
+    );
+  }
 
   deleteProduct(product: Product) {
-    this.products = this.products.filter((prod) => {
-      return prod !== product;
-    });
-    console.log(this.products);
+    this.service.deleteProduct(product);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  products: Product[] = [
+    {
+      id: '1',
+      title: 'Voda',
+      price: 40,
+      image:
+        'https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: '2',
+      title: 'Sok',
+      price: 60,
+      image: 'https://nsnbc.me/wp-content/uploads/2019/07/fruit-juice.jpg',
+    },
+    {
+      id: '3',
+      title: 'Špagete',
+      price: 80,
+      image:
+        'https://pbs.twimg.com/profile_images/423290202443251712/HYR23k2c.jpeg',
+    },
+    {
+      id: '4',
+      title: 'Makarone',
+      price: 90,
+      image: 'https://bonapeti.rs/files/lib/600x350/makaroni-vkusensos1.jpg',
+    },
+  ];
+
+  productSubject = new BehaviorSubject<Product[]>(this.products);
+
+  constructor() {}
+
+  deleteProduct(product: Product) {
+    this.products = this.products.filter((prod) => {
+      return prod !== product;
+    });
+    this.productSubject.next(this.products);
+  }
+}
